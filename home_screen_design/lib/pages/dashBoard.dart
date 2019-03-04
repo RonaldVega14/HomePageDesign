908bfb7c835deb3f6b05eedf4a1e4827036e268a
Ronald Vega <ronald.vega48@gmail.com>
2019-03-04 13:21:47 -0600

    Adjusting the code

--- home_screen_design/lib/pages/dashBoard.dart
+++ home_screen_design/lib/pages/dashBoard.dart
@@ -1,152 +1,187 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'dart:ui' as ui;
-import 'package:home_screen_design/pages/my_flutter_app_icons.dart' as CustomIcons;
+import 'package:home_screen_design/pages/my_flutter_app_icons.dart'
+    as CustomIcons;
 
 class Dashboard extends StatefulWidget {
   @override
   _DashboardState createState() => new _DashboardState();
-    }
-  
-  class _DashboardState extends State<Dashboard> with TickerProviderStateMixin {
-  
+}
+
+class _DashboardState extends State<Dashboard> with TickerProviderStateMixin {
   //Card item
-  Widget myItems(IconData icon, String title, Color color){
-      return Material(
+  Widget myItems(IconData icon, String title, Color color) {
+    return Material(
+      color: Colors.transparent,
+      child: Container(
         color: Colors.transparent,
-        child: Container(
-            color: Colors.transparent,
-            child: InkWell(
-              borderRadius: BorderRadius.circular(6.0),
-              highlightColor: color.withOpacity(1.0),
-              onTap: (){},
-              child:Material(
-                color: Colors.white.withOpacity(0.9),
-                elevation: 12.0,
-                shadowColor: Colors.black,
-                borderRadius: BorderRadius.circular(6.0),
-                child: Center(
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Flexible(child: _buildColumn(title, icon, color), flex: 1, fit: FlexFit.tight,)
-                      ],
-                    ),
-                ),
+        child: InkWell(
+          borderRadius: BorderRadius.circular(6.0),
+          highlightColor: color.withOpacity(1.0),
+          onTap: () {},
+          child: Material(
+            color: Colors.white.withOpacity(0.9),
+            elevation: 12.0,
+            shadowColor: Colors.black,
+            borderRadius: BorderRadius.circular(6.0),
+            child: Center(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Flexible(
+                    child: _buildColumn(title, icon, color),
+                    flex: 1,
+                    fit: FlexFit.tight,
+                  )
+                ],
+              ),
             ),
           ),
+        ),
       ),
-    ); 
+    );
   }
 
   //Construye el texto centrado
-  Widget _buildText(String title, Color color){
+  Widget _buildText(String title, Color color) {
     return Center(
-      child:Text(
-        title,
-        textAlign:TextAlign.center,
-        style:style.copyWith(color: color)
-          ),
-      );
+      child: Text(title,
+          textAlign: TextAlign.center, style: style.copyWith(color: color)),
+    );
   }
+
 //Construye la fila del texto
-  Widget _buildTextRow(String title, Color color){
+  Widget _buildTextRow(String title, Color color) {
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
       crossAxisAlignment: CrossAxisAlignment.center,
       children: <Widget>[
-        Flexible(child: Container(color: Colors.transparent),flex: 1, fit: FlexFit.tight),
-        Flexible(child: _buildText(title, color),flex: 3, fit: FlexFit.loose,),
-        Flexible(child: Container(color: Colors.transparent),flex: 1, fit: FlexFit.tight)
+        Flexible(
+            child: Container(color: Colors.transparent),
+            flex: 1,
+            fit: FlexFit.tight),
+        Flexible(
+          child: _buildText(title, color),
+          flex: 3,
+          fit: FlexFit.loose,
+        ),
+        Flexible(
+            child: Container(color: Colors.transparent),
+            flex: 1,
+            fit: FlexFit.tight)
       ],
-
     );
   }
+
 //Construye el icono
-  Widget _buildIcon(IconData icon, Color color){
-    return  Padding(
-        padding: const EdgeInsets.all(15.0),
-        child: Icon(
-          icon,
-          color: color,
-          size: MediaQuery.of(context).size.height*0.07,
-        ),
-    ); 
+  Widget _buildIcon(IconData icon, Color color) {
+    return Padding(
+      padding: const EdgeInsets.all(15.0),
+      child: Icon(
+        icon,
+        color: color,
+        size: MediaQuery.of(context).size.height * 0.07,
+      ),
+    );
   }
+
 //Construye las columnas
-  Widget _buildColumn(String title, IconData icon, Color color){
+  Widget _buildColumn(String title, IconData icon, Color color) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.center,
       children: <Widget>[
-      //Icon
-      Flexible(child: _buildIcon(icon, color), flex: 3, fit: FlexFit.loose),
-      //Text
-      Flexible(child: _buildTextRow(title, color), flex: 1, fit: FlexFit.loose),
+        //Icon
+        Flexible(child: _buildIcon(icon, color), flex: 3, fit: FlexFit.loose),
+        //Text
+        Flexible(
+            child: _buildTextRow(title, color), flex: 1, fit: FlexFit.loose),
       ],
     );
   }
 
 //Construye el perfil
-Widget _buildProfile(IconData icon, String title, Color color){
-  return Material(
+  Widget _buildProfile(IconData icon, String title, Color color) {
+    return Material(
+      color: Colors.transparent,
+      child: Container(
         color: Colors.transparent,
-        child: Container(
-            color: Colors.transparent,
-            child: InkWell(
+        child: InkWell(
+          borderRadius: BorderRadius.circular(6.0),
+          highlightColor: color.withOpacity(1.0),
+          onTap: () {},
+          child: Material(
+              color: Colors.white.withOpacity(0.9),
+              elevation: 12.0,
+              shadowColor: Colors.black,
               borderRadius: BorderRadius.circular(6.0),
-              highlightColor: color.withOpacity(1.0),
-              onTap: (){},
-              child:Material(
-                color: Colors.white.withOpacity(0.9),
-                elevation: 12.0,
-                shadowColor: Colors.black,
-                borderRadius: BorderRadius.circular(6.0),
-                child: Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: <Widget>[
-                        Flexible(child: Container(
-                          child: Text(title, style: style.copyWith(color: color), textAlign: TextAlign.center,
-                        )), flex: 3, fit: FlexFit.tight,),
-                        Flexible(child:Center(child:Container(child: Icon(icon, color: color,), margin: const EdgeInsets.all(5.0),)),flex: 1, fit: FlexFit.tight,)
-                      ]
-                  )
-                ),
-            ),
-          ),
-      );
-}
+              child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Flexible(
+                      child: Container(
+                          child: Text(
+                        title,
+                        style: style.copyWith(color: color),
+                        textAlign: TextAlign.center,
+                      )),
+                      flex: 3,
+                      fit: FlexFit.tight,
+                    ),
+                    Flexible(
+                      child: Center(
+                          child: Container(
+                        child: Icon(
+                          icon,
+                          color: color,
+                        ),
+                        margin: const EdgeInsets.all(5.0),
+                      )),
+                      flex: 1,
+                      fit: FlexFit.tight,
+                    )
+                  ])),
+        ),
+      ),
+    );
+  }
 
 //Junta y construye todo el contenido
-  Widget _buildContent(){
+  Widget _buildContent() {
     return StaggeredGridView.count(
       crossAxisCount: 2,
       crossAxisSpacing: 10.0,
       mainAxisSpacing: 10.0,
-      padding: const EdgeInsets.symmetric(vertical: 9.0, horizontal:16.0),
-      
+      padding: const EdgeInsets.symmetric(vertical: 9.0, horizontal: 16.0),
       children: <Widget>[
         Container(),
-        myItems(CustomIcons.MyFlutterApp.money, 'Movimientos en bolsa', Colors.blue[900]),
-        myItems(CustomIcons.MyFlutterApp.chart_bars, 'Movimientos en fondo', Colors.blue[900]),
-        myItems(CustomIcons.MyFlutterApp.database_1, 'Movimientos consolidados', Colors.blue[900]),
-        myItems(CustomIcons.MyFlutterApp.book, 'Estado de Cuenta', Colors.blue[900]),
-        _buildProfile(CustomIcons.MyFlutterApp.user, 'Ronald Vega', Colors.blue[900])
+        myItems(CustomIcons.MyFlutterApp.money, 'Movimientos en bolsa',
+            Color.fromRGBO(23, 17, 85, 1)),
+        myItems(CustomIcons.MyFlutterApp.chart_bars, 'Movimientos en fondo',
+            Color.fromRGBO(23, 17, 85, 1)),
+        myItems(CustomIcons.MyFlutterApp.database_1, 'Movimientos consolidados',
+            Color.fromRGBO(23, 17, 85, 1)),
+        myItems(CustomIcons.MyFlutterApp.book, 'Estado de Cuenta',
+            Color.fromRGBO(23, 17, 85, 1)),
+        _buildProfile(CustomIcons.MyFlutterApp.user, 'Ronald Vega',
+            Color.fromRGBO(23, 17, 85, 1))
       ],
       staggeredTiles: [
-        StaggeredTile.extent(2, MediaQuery.of(context).size.height*0.20),
-        StaggeredTile.extent(1, MediaQuery.of(context).size.height*0.25),
-        StaggeredTile.extent(1, MediaQuery.of(context).size.height*0.25),
-        StaggeredTile.extent(1, MediaQuery.of(context).size.height*0.25),
-        StaggeredTile.extent(1, MediaQuery.of(context).size.height*0.25),
-        StaggeredTile.extent(2, MediaQuery.of(context).size.height*0.10),
+        StaggeredTile.extent(2, MediaQuery.of(context).size.height * 0.20),
+        StaggeredTile.extent(1, MediaQuery.of(context).size.height * 0.25),
+        StaggeredTile.extent(1, MediaQuery.of(context).size.height * 0.25),
+        StaggeredTile.extent(1, MediaQuery.of(context).size.height * 0.25),
+        StaggeredTile.extent(1, MediaQuery.of(context).size.height * 0.25),
+        StaggeredTile.extent(2, MediaQuery.of(context).size.height * 0.10),
       ],
     );
   }
-  
-  TextStyle style =TextStyle(fontFamily: 'Montserrat', fontSize: 14.0, color: Colors.black);
+
+  TextStyle style =
+      TextStyle(fontFamily: 'Montserrat', fontSize: 14.0, color: Colors.black);
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -156,9 +191,10 @@ Widget _buildProfile(IconData icon, String title, Color color){
             fit: StackFit.loose,
             children: <Widget>[
               Container(
-                height: MediaQuery.of(context).size.height*0.15,
-                color: Colors.blue[900],
-                child: _buildText('Bienvenido a Servicios Generales Bursatiles', Colors.white),
+                height: MediaQuery.of(context).size.height * 0.15,
+                color: Color.fromRGBO(23, 17, 85, 1),
+                child: _buildText('Bienvenido a Servicios Generales Bursatiles',
+                    Colors.white),
               ),
               _buildContent(),
             ],
@@ -167,6 +203,4 @@ Widget _buildProfile(IconData icon, String title, Color color){
       ),
     );
   }
-
 }
-  
